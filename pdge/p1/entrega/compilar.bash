cbeda0bd052cea6e8a4f271934b931c718829156
mariabarrosoh <mbarrosohonrubia@gmail.com>
2021-10-07 16:29:05 +0200

    PDGE: add entrega directory to end practica

---
+++ pdge/p1/entrega/compilar.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+file=$1
+
+HADOOP_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath)
+echo $HADOOP_CLASSPATH
+
+rm -rf ${file}
+mkdir -p ${file}
+
+javac -classpath $HADOOP_CLASSPATH -d ${file} ${file}.java
+jar -cvf ${file}.jar -C ${file} .
--- pdge/p1/hadoop/opcionales/ClubAgeDesc/clean_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-col_list = ['Name','Age','Club']
-df = pd.read_csv("original_clubagedata.csv", usecols=col_list)
-df.dropna(inplace=True)
-df.to_csv('clubagedata.csv', sep=',',header=None, index=False)

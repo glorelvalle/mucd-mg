24907cccc583a7814c13bb630a2ad57fe7aca89e
mariabarrosoh <mbarrosohonrubia@gmail.com>
2021-10-08 13:42:06 +0200

    PDGE: Add wo argument compilar.bash

--- pdge/p1/entrega/compilar.bash
+++ pdge/p1/entrega/compilar.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 file=$1
+name=$2
 
 HADOOP_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath)
 echo $HADOOP_CLASSPATH
@@ -9,4 +10,4 @@ rm -rf ${file}
 mkdir -p ${file}
 
 javac -classpath $HADOOP_CLASSPATH -d ${file} ${file}.java
-jar -cvf ${file}.jar -C ${file} .
+jar -cvf ${name}.jar -C ${file} .

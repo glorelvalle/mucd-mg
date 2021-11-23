3e116607326fd04050e103f7da3703e9e3fac3f5
glorelvalle <glorelvalle@gmail.com>
2021-11-23 13:55:23 +0100

    ej7 pst

---
+++ pst/p1_pst/ej7.m
@@ -0,0 +1,42 @@
+%% Ejercicio 7: transformaciones de la variable independiente
+
+nx=[-3:11]
+x=[zeros(1, 3) 2 0 1 -1 3 zeros(1,7)]
+
+figure;
+stem(nx, x);
+title('Señal discreta X[n]');
+xlabel('Tiempo (Discreto)');
+ylabel('Valor');
+
+figure;
+subplot(2,2,1);
+y1=x;
+ny1=nx+2;
+stem(ny1,y1);
+title('x retrasada 2');
+xlabel('ny1');
+
+subplot(2,2,2);
+y2=x;
+ny2=nx-1;
+stem(ny2,y2);
+title('x adelantada 1');
+xlabel('ny2');
+
+subplot(2,2,3);
+y3=x
+ny3=-nx
+%ny3=fliplr(x)
+stem(ny3,y3);
+title('x invertida');
+xlabel('ny3');
+
+subplot(2,2,4);
+y4=x
+ny4=-nx+1
+stem(ny4,y4);
+title('x invertida y adelantada 1');
+xlabel('ny4');
+%ny3=fliplr(nx) - 8
+

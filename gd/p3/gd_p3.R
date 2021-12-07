6a39a2256e8048a6c1edd8a2f3f93e6849f5d1e7
glorelvalle <glorelvalle@gmail.com>
2021-12-07 22:12:14 +0100

    con imagenes automaticas

--- gd/p3/gd_p3.R
+++ gd/p3/gd_p3.R
@@ -16,6 +16,7 @@ freqs <- data[,2:ncol(data)]
 unique_freqs <- data.frame(table(unlist(freqs)))
 
 # Representamos un histograma con los datos correspondientes a las tarjetas
+jpeg(file.path("img", paste("histograma", ".png", sep = "")))
 ggplot(unique_freqs, aes(x=Var1, y=Freq)) + 
   geom_bar(stat="identity", color="black", fill=c("steelblue4", "darkmagenta"))+
   geom_text(aes(label=Freq), vjust=1.6, color="white", size=4.5)+
@@ -27,18 +28,23 @@ ggplot(unique_freqs, aes(x=Var1, y=Freq)) +
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank())
+dev.off()
 
 # Calculamos la distancia euclídea entre columnas
 distances <- as.matrix(dist(t(freqs), method="euclidean"))
 
 # Mapa de calor correspondiente
+jpeg(file.path("img", paste("mapa_calor", ".png", sep = "")))
 my_palette <- colorRampPalette(c("darkmagenta", "steelblue4", "lightsteelblue"))
 heatmap.2(distances, col=my_palette, dendrogram='row', Rowv=TRUE, Colv=TRUE, trace='none', symkey=FALSE)
 title("Mapa de calor de las tarjetas")
+dev.off()
 
 # Obtenemos el grafo ponderado de la similud entre tarjetas
+jpeg(file.path("img", paste("grafo", ".png", sep = "")))
 qgraph(1/(1 + distances), labels=colnames(distances), layout="spring", vsize=7, colors=c("darkmagenta", "steelblue3", "lightsteelblue"), edge.color="plum3")
 title("Grafo ponderado de la similitud entre tarjetas")
+dev.off()
 
 # Obtenemos las tarjetas más relacionadas
 which(min(dist(t(freqs), method="euclidean")) == distances, arr.ind=TRUE)
